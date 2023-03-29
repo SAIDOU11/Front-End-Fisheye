a704bf0e3dd302646719664cb6394b4da4ff837d
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-03-29 09:12:17 +0200

    avancement

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,18 +1,30 @@
 async function getPhotographers() {
   // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
   // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-  // et bien retourner le tableau photographers seulement une fois récupéré
 
-  const apiUrl = "./data/photographers.json";
-
-  const response = await fetch(apiUrl);
-  const data = await response.json();
-  console.log(typeof data);
+  let apiUrl = "./data/photographers.json";
+  let response = await fetch(apiUrl);
+  console.log(response);
+  let data = await response.json();
+  console.log(data);
   console.log(data.photographers);
   console.log(data.media);
-  console.log(data.photographers[0]);
+  console.log(data.photographers[0].country);
+  console.log(data.photographers[1].country);
+  data.map((values) => {
+    let display = "";
+    display = `<div class="photographer_section">
+    <article class="thumb-photographer">
+    <img src=${values.portrait} alt="" class="user" />
+    <h2>${values.name}</h2>
+    <p class="city-country"></p>
+    <p class="tagline"></p>
+    <p class="price"></p>
+  </article>
+  </div>`;
+  });
 
-  //     {
+  //   let photographers = [{
   //       name: "Ma data test",
   //       id: 1,
   //       city: "Paris",
@@ -31,12 +43,16 @@ async function getPhotographers() {
   //       portrait: "account.png",
   //     },
   //   ];
+
+  // ********************************************************************************************************************
   // et bien retourner le tableau photographers seulement une fois récupéré
+
+  // ********************************************************************************************************************
   //   return {
   //     photographers: [...photographers, ...photographers, ...photographers],
   //   };
 }
-getPhotographers().then(alert);
+getPhotographers();
 
 // async function displayData(photographers) {
 //   const photographersSection = document.querySelector(".photographer_section");
