e077d1e2a0a47f2585ba854ef63d7fa5d5b377ce
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-03-31 19:26:23 +0200

    home page réglage

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -7,20 +7,23 @@ function photographerFactory(data) {
   function getUserCardDOM() {
     const article = document.createElement("article");
     const a = document.createElement("a");
+    const a2 = document.createElement("a");
     const img = document.createElement("img");
+    const h2 = document.createElement("h2");
     a.setAttribute("href", "photographer.html");
+
     img.setAttribute("src", picture);
-    // img.setAttribute("class", "user");
     img.setAttribute("id", id);
     img.setAttribute("alt", name);
-    const h2 = document.createElement("h2");
+    a2.setAttribute("href", "photographer.html");
     h2.textContent = name;
     h2.setAttribute("href", "photographer.html");
     h2.setAttribute("alt", name);
     h2.setAttribute("id", id);
     article.appendChild(a);
     a.appendChild(img);
-    a.appendChild(h2);
+    article.appendChild(a2);
+    a2.appendChild(h2);
     const pa1 = document.createElement("p");
     article.appendChild(pa1);
     pa1.textContent = `${city}, ${country}`;
