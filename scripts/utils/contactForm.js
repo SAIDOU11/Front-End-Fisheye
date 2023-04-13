8ba8190a4adb10b6092d48a9c0cacea8752c4217
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-04-13 22:13:41 +0200

    modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,13 @@
+const contactBtn = document.querySelectorAll(".contact_button");
+
+contactBtn.forEach((btn) => btn.addEventListener("click", displayModal));
+
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "block";
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "none";
 }
