d86e0e565467682aaeb78302c017b700e257646a
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-05-27 18:22:50 +0200

    options

--- scripts/utils/dropdowns.js
+++ scripts/utils/dropdowns.js
@@ -1,10 +1,13 @@
 const dropdowns = document.querySelectorAll(".dropdown");
+console.log(dropdowns);
 
 dropdowns.forEach((dropdown) => {
-  const select = dropdown.querySelector(".select-button");
+  const select = dropdown.querySelector(".select");
   const arrow = dropdown.querySelector(".arrow");
   const menu = dropdown.querySelector(".menu");
   const options = dropdown.querySelectorAll(".menu li");
+  console.log(options, options.length);
+
   const selected = dropdown.querySelector(".selected");
 
   select.addEventListener("click", () => {
@@ -15,11 +18,13 @@ dropdowns.forEach((dropdown) => {
   // loop options
   options.forEach((option) => {
     option.addEventListener("click", () => {
+      console.log("OPTIONS ?? ");
       selected.innerText = option.innerText;
       select.classList.remove("select-clicked");
       arrow.classList.remove("arrow-rotate");
       menu.classList.remove("menu-open");
       options.forEach((option) => {
+        console.log("OPTIONS ?? ");
         option.classList.remove("active");
       });
       option.classList.add("active");
