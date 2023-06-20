69af06b170e76f5b0c480190ab80d1d70a8c44a5
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-06-20 15:02:00 +0200

    solutions

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -14,20 +14,26 @@ async function getDataMedia() {
   // Filtre Médias
   let filterId = media;
   let mediaProfil = filterId.filter((obj) => obj.photographerId == numberId);
-  console.log(mediaProfil);
-  mediaProfil.sort(byLikes);
+  // mediaProfil.sort(byLikes);
+  // mediaProfil.sort(byLikes);
+  document.addEventListener("click", (e) => {
+    console.log(e.target.dataset.date);
+    if (e.target.dataset.date === "date") {
+      mediaProfil.sort(byDate);
+    }
+    if (e.target.dataset.likes === "likes") {
+      return mediaProfil.sort(byLikes);
+    }
+    if (e.target.dataset.title === "title") {
+      mediaProfil.sort(byTitle);
+    }
+  });
 
-  // console.log("\n\n Sort by popularité ");
-  // console.log(mediaProfil.sort(byLikes));
-  // console.log("\n\n Sort by date ");
-  // console.log(mediaProfil.sort(byDate));
-  // console.log("\n\n Sort by title ");
-  // console.log(mediaProfil.sort(byTitle));
   return { medias: [...mediaProfil], photographers: [...photograhProfil] };
 }
 
 async function displayLightbox(medias) {
-  console.log(medias);
+  // console.log(medias);
   document.querySelectorAll(".divContent").forEach((document) => {
     document.addEventListener("click", (e) => {
       console.log(e);
@@ -79,3 +85,56 @@ async function init() {
 }
 
 init();
+
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+  const select = dropdown.querySelector(".select");
+  const arrow = dropdown.querySelector(".arrow");
+  const menu = dropdown.querySelector(".menu");
+  const options = dropdown.querySelectorAll(".menu li");
+  const selected = dropdown.querySelector(".selected");
+  // Event select element
+  arrow.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      select.classList.toggle("select-clicked");
+      arrow.classList.toggle("arrow-rotate");
+      menu.classList.toggle("menu-open");
+    }
+  });
+  select.addEventListener("click", (e) => {
+    // console.log(e);
+    select.classList.toggle("select-clicked");
+    arrow.classList.toggle("arrow-rotate");
+    menu.classList.toggle("menu-open");
+  });
+  // loop options
+  options.forEach((option) => {
+    option.addEventListener("click", (e) => {
+      // console.log(e, mediaProfil);
+      // console.log(option.innerText);
+      selected.innerText = option.innerText;
+      if (selected.innerText === "Popularité") {
+        console.log("Popularité !!!!!!!!!!!!!!!!");
+      }
+      if (selected.innerText === "Date") {
+        console.log("Date !!!!!!!!!!!!!!!!");
+      }
+      if (selected.innerText === "Titre") {
+        console.log("Titre !!!!!!!!!!!!!!!!");
+      }
+      select.classList.remove("select-clicked");
+      arrow.classList.remove("arrow-rotate");
+      menu.classList.remove("menu-open");
+      options.forEach((option) => {
+        option.classList.remove("active");
+      });
+      option.classList.add("active");
+    });
+  });
+});
+
+function byDate(a, b) {
+  // Chronologically by year, month then day
+  return new Date(a.date).valueOf() - new Date(b.date).valueOf();
+}
--- scripts/utils/dropdowns.js
+++ scripts/utils/dropdowns.js
@@ -1,49 +1,49 @@
-const dropdowns = document.querySelectorAll(".dropdown");
+// const dropdowns = document.querySelectorAll(".dropdown");
 
-dropdowns.forEach((dropdown) => {
-  const select = dropdown.querySelector(".select");
-  const arrow = dropdown.querySelector(".arrow");
-  const menu = dropdown.querySelector(".menu");
-  const options = dropdown.querySelectorAll(".menu li");
-  const selected = dropdown.querySelector(".selected");
-  // Event select element
-  arrow.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-      select.classList.toggle("select-clicked");
-      arrow.classList.toggle("arrow-rotate");
-      menu.classList.toggle("menu-open");
-    }
-  });
-  select.addEventListener("click", (e) => {
-    console.log(e.target, e);
-    select.classList.toggle("select-clicked");
-    arrow.classList.toggle("arrow-rotate");
-    menu.classList.toggle("menu-open");
-  });
-  // loop options
-  options.forEach((option) => {
-    option.addEventListener("click", () => {
-      console.log(option.innerText);
-      selected.innerText = option.innerText;
-      if (selected.innerText === "Popularité") {
-        console.log("Popularité !!!!!!!!!!!!!!!!");
-      }
-      if (selected.innerText === "Date") {
-        console.log("Date !!!!!!!!!!!!!!!!");
-      }
-      if (selected.innerText === "Titre") {
-        console.log("Titre !!!!!!!!!!!!!!!!");
-      }
-      select.classList.remove("select-clicked");
-      arrow.classList.remove("arrow-rotate");
-      menu.classList.remove("menu-open");
-      options.forEach((option) => {
-        option.classList.remove("active");
-      });
-      option.classList.add("active");
-    });
-  });
-});
+// dropdowns.forEach((dropdown) => {
+//   const select = dropdown.querySelector(".select");
+//   const arrow = dropdown.querySelector(".arrow");
+//   const menu = dropdown.querySelector(".menu");
+//   const options = dropdown.querySelectorAll(".menu li");
+//   const selected = dropdown.querySelector(".selected");
+//   // Event select element
+//   arrow.addEventListener("keydown", (e) => {
+//     if (e.key === "Enter") {
+//       select.classList.toggle("select-clicked");
+//       arrow.classList.toggle("arrow-rotate");
+//       menu.classList.toggle("menu-open");
+//     }
+//   });
+//   select.addEventListener("click", (e) => {
+//     console.log(e.target, e);
+//     select.classList.toggle("select-clicked");
+//     arrow.classList.toggle("arrow-rotate");
+//     menu.classList.toggle("menu-open");
+//   });
+//   // loop options
+//   options.forEach((option) => {
+//     option.addEventListener("click", () => {
+//       console.log(option.innerText);
+//       selected.innerText = option.innerText;
+//       if (selected.innerText === "Popularité") {
+//         console.log("Popularité !!!!!!!!!!!!!!!!");
+//       }
+//       if (selected.innerText === "Date") {
+//         console.log("Date !!!!!!!!!!!!!!!!");
+//       }
+//       if (selected.innerText === "Titre") {
+//         console.log("Titre !!!!!!!!!!!!!!!!");
+//       }
+//       select.classList.remove("select-clicked");
+//       arrow.classList.remove("arrow-rotate");
+//       menu.classList.remove("menu-open");
+//       options.forEach((option) => {
+//         option.classList.remove("active");
+//       });
+//       option.classList.add("active");
+//     });
+//   });
+// });
 
 function byTitle(a, b) {
   // Alphabetically by Title
