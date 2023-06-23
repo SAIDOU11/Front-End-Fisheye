932b078539b9528538f458d6fae1c04813715134
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-06-23 18:24:35 +0200

    localecompare

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -56,6 +56,8 @@ async function displayLightbox(medias) {
 async function displayData(medias, photographers) {
   const headerProfil = document.querySelector(".photograph-header");
   const mediasSection = document.querySelector(".media_section");
+  headerProfil.innerHTML = "";
+  mediasSection.innerHTML = "";
 
   photographers.forEach((photographer) => {
     const photographerModel = photographerFactory(photographer);
@@ -82,58 +84,65 @@ async function init() {
   const { medias, photographers } = await getDataMedia();
   displayData(medias, photographers);
   displayLightbox(medias);
+  filterFeature(medias, photographers);
 }
 
 init();
 
-const dropdowns = document.querySelectorAll(".dropdown");
-
-dropdowns.forEach((dropdown) => {
-  const select = dropdown.querySelector(".select");
-  const arrow = dropdown.querySelector(".arrow");
-  const menu = dropdown.querySelector(".menu");
-  const options = dropdown.querySelectorAll(".menu li");
-  const selected = dropdown.querySelector(".selected");
-  // Event select element
-  arrow.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
+function filterFeature(medias, photographers) {
+  const dropdowns = document.querySelectorAll(".dropdown");
+  dropdowns.forEach((dropdown) => {
+    const select = dropdown.querySelector(".select");
+    const arrow = dropdown.querySelector(".arrow");
+    const menu = dropdown.querySelector(".menu");
+    const options = dropdown.querySelectorAll(".menu li");
+    const selected = dropdown.querySelector(".selected");
+    // Event select element
+    arrow.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        select.classList.toggle("select-clicked");
+        arrow.classList.toggle("arrow-rotate");
+        menu.classList.toggle("menu-open");
+      }
+    });
+    select.addEventListener("click", (e) => {
+      // console.log(e);
       select.classList.toggle("select-clicked");
       arrow.classList.toggle("arrow-rotate");
       menu.classList.toggle("menu-open");
-    }
-  });
-  select.addEventListener("click", (e) => {
-    // console.log(e);
-    select.classList.toggle("select-clicked");
-    arrow.classList.toggle("arrow-rotate");
-    menu.classList.toggle("menu-open");
-  });
-  // loop options
-  options.forEach((option) => {
-    option.addEventListener("click", (e) => {
-      // console.log(e, mediaProfil);
-      // console.log(option.innerText);
-      selected.innerText = option.innerText;
-      if (selected.innerText === "Popularité") {
-        console.log("Popularité !!!!!!!!!!!!!!!!");
-      }
-      if (selected.innerText === "Date") {
-        console.log("Date !!!!!!!!!!!!!!!!");
-      }
-      if (selected.innerText === "Titre") {
-        console.log("Titre !!!!!!!!!!!!!!!!");
-      }
-      select.classList.remove("select-clicked");
-      arrow.classList.remove("arrow-rotate");
-      menu.classList.remove("menu-open");
-      options.forEach((option) => {
-        option.classList.remove("active");
+    });
+    // loop options
+    options.forEach((option) => {
+      option.addEventListener("click", (e) => {
+        // console.log(e, mediaProfil);
+        // console.log(option.innerText);
+        selected.innerText = option.innerText;
+        if (selected.innerText === "Popularité") {
+          // medias.sort((a, b) => a.likes.localeCompare(b.likes));
+          // displayData(medias, photographers);
+          // console.log("Popularité !!!!!!!!!!!!!!!!");
+        }
+        if (selected.innerText === "Date") {
+          // medias.sort((a, b) => a.date.localeCompare(b.date));
+          // displayData(medias, photographers);
+          // console.log("Date !!!!!!!!!!!!!!!!");
+        }
+        if (selected.innerText === "Titre") {
+          medias.sort((a, b) => a.title.localeCompare(b.title));
+          displayData(medias, photographers);
+          console.log(medias, photographers, "Titre !!!!!!!!!!!!!!!!");
+        }
+        select.classList.remove("select-clicked");
+        arrow.classList.remove("arrow-rotate");
+        menu.classList.remove("menu-open");
+        options.forEach((option) => {
+          option.classList.remove("active");
+        });
+        option.classList.add("active");
       });
-      option.classList.add("active");
     });
   });
-});
-
+}
 function byDate(a, b) {
   // Chronologically by year, month then day
   return new Date(a.date).valueOf() - new Date(b.date).valueOf();
